97441b6a272a8dccff91c4a21d155d543629fb54
sahithi-pathro <sahithipathro@gmail.com>
2026-04-28 15:02:25 +0530

    Add files via upload

---
+++ red.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the value in a:\n");
+scanf("%d",&a);
+printf("enter the value in b:\n");
+scanf("%d",&b);
+printf("enter the value in c:\n");
+scanf("%d",&c);
+if(a==2&&b==2&c==2)
+{
+printf("10");
+else if(a==b&&b==c&&c==a)
+{
+printf("5");
+else if(b!=a&&c!=a)
+{
+printf("1");
+}
+else
+{
+printf("0");
+}
+}
